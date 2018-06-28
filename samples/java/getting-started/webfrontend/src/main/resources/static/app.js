d5018ba53b775ff2c6b60f6d657fbded9cc11a99
andxu <andxu@microsoft.com>
2018-06-28 16:33:41 +0800

    modify samples the same as node.js

--- samples/java/getting-started/webfrontend/src/main/resources/static/app.js
+++ samples/java/getting-started/webfrontend/src/main/resources/static/app.js
@@ -4,7 +4,7 @@ app.controller('MainController', function($scope, $http) {
 
     $scope.messages = [];
     $scope.sayHelloToServer = function() {
-        $http.get("/api?_=" + Date.now(), {responseType: 'text'}).then(function(response) {
+        $http.get("/greeting?_=" + Date.now(), {responseType: 'text'}).then(function(response) {
             $scope.messages.push(response.data);
         }, function(error) {
             cosnole.log(error);
